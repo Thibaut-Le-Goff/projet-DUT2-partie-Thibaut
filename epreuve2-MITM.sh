4cbbd528e84ca6a4f29fcb3a5de37ab0823ef5c3
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-04-27 17:50:19 +0200

    Update epreuve2-MITM.sh

--- epreuve2-MITM.sh
+++ epreuve2-MITM.sh
@@ -1,1 +1,12 @@
-docker network create --subnet 172.50.0.0/16 MITM && docker pull ctfiutbeziers/m-alice && docker run --net MITM --ip 172.50.0.2 --cap-add=NET_ADMIN --name alice-red-man --hostname Red-man ctfiutbeziers/m-alice & docker pull ctfiutbeziers/m-bob && docker run --net MITM --ip 172.50.0.3 --cap-add=NET_ADMIN --name bob-red-man --hostname Red-man ctfiutbeziers/m-bob & docker pull ctfiutbeziers/m-eve && docker run --net MITM --ip 172.50.0.4 --cap-add=NET_ADMIN -it --name eve-red-man --hostname Red-man ctfiutbeziers/m-eve
+#!/bin/bash
+
+docker network create --subnet 172.50.0.0/16 MITM &&
+
+	docker pull ctfiutbeziers/m-alice && 
+	docker run --net MITM --ip 172.50.0.2 --cap-add=NET_ADMIN --name alice-red-man --hostname Red-man ctfiutbeziers/m-alice &
+
+docker pull ctfiutbeziers/m-bob && 
+	docker run --net MITM --ip 172.50.0.3 --cap-add=NET_ADMIN --name bob-red-man --hostname Red-man ctfiutbeziers/m-bob & 
+
+docker pull ctfiutbeziers/m-eve && 
+	docker run --net MITM --ip 172.50.0.4 --cap-add=NET_ADMIN -it --name eve-red-man --hostname Red-man ctfiutbeziers/m-eve
